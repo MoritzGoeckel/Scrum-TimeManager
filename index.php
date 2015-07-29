b3a4303baf8c77cb2223768136ebcc64620e7ece
Koding <moritz.goeckel@gmail.com>
2015-07-29 14:27:23 +0000

    Working on mysql

--- index.php
+++ index.php
@@ -15,43 +15,38 @@ mysql_select_db("timemgr") or die("MYSQL CONNECTION ERROR");
 $app->get(
     '/',
     function () {
-        echo "Hallo Welt!";
-        
-        $query = "SELECT * FROM projects";
-        $result = mysql_query($query) or die("MYSQL ERROR: " . mysql_error());
-        
-        while ($line = mysql_fetch_array($result)) {
-            echo $line['id'];
-        }
+        echo "Here the docu will apear sometimes... maybe";
     }
 );
+
+$app->get('/projects/:uid', function ($uid) {
+    
+    $query = "SELECT * FROM projects, user_in_project, user
+    LEFT JOIN user ON (user.id = projects.author)
+    WHERE user_in_project.user = " . $uid . " AND user_in_project.project = projects.id";
+    
+    $result = mysql_query($query) or die("MYSQL ERROR: " . mysql_error());
+    
+    $output = array();
+    while ($line = mysql_fetch_array($result)) {
+        array_push($output, array(
+            'id' => $line['id'],
+            'name' => $line['name']
+        ));
+    }
+    
+    echo json_encode($output);
+});
+
+$app->get('/tasks/:uid', function ($uid) {
+    
+});
 
 // POST route
 /*$app->post(
     '/post',
     function () {
         echo 'This is a POST route';
-    }
-);
-
-// PUT route
-$app->put(
-    '/put',
-    function () {
-        echo 'This is a PUT route';
-    }
-);
-
-// PATCH route
-$app->patch('/patch', function () {
-    echo 'This is a PATCH route';
-});
-
-// DELETE route
-$app->delete(
-    '/delete',
-    function () {
-        echo 'This is a DELETE route';
     }
 );*/
 
