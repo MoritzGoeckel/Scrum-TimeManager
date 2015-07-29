959c8b31cc13ee8a06cf063a69bbd6b05c7e03f2
Koding <moritz.goeckel@gmail.com>
2015-07-29 13:39:28 +0000

    SOSO

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
 <?php
 
+require 'config.php';
 require 'Slim/Slim/Slim.php';
 
 \Slim\Slim::registerAutoloader();
 
 $app = new \Slim\Slim();
 
-// GET route
+//DB
+$db = mysql_connect("localhost", "root", $database_password)
+    or die("Keine Verbindung möglich: " . mysql_error());
+mysql_select_db("timemgr") or die("MYSQL CONNECTION ERROR");
+
 $app->get(
     '/',
     function () {
         echo "Hallo Welt!";
+        
+        $query = "SELECT * FROM projects";
+        $result = mysql_query($query) or die("MYSQL ERROR: " . mysql_error());
+        
+        while ($line = mysql_fetch_array($result)) {
+            echo $line['id'];
+        }
     }
 );
 
 // POST route
-$app->post(
+/*$app->post(
     '/post',
     function () {
         echo 'This is a POST route';
@@ -41,6 +53,9 @@ $app->delete(
     function () {
         echo 'This is a DELETE route';
     }
-);
+);*/
 
 $app->run();
+
+//mysql_free_result($result);
+//mysql_close($db);
