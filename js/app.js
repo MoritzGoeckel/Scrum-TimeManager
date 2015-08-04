e45fbeb6489721bd36ee69967fa688c87c4d14c7
Koding <moritz.goeckel@gmail.com>
2015-08-04 15:33:11 +0000

    Angular now working

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 var app = angular.module("app", ['ngRoute']);
       
 app.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-           when('/login', {
-              templateUrl: 'login',
-              controller: 'loginCtl'
-           }).
-           when('/dashboard', {
-              templateUrl: 'dashboard',
-              controller: 'dashboardCtl'
-           }).
-           otherwise({
-              redirectTo: '/dashboard'
-           });
-    }]);
-    
-app.controller('loginCtl', function($scope) {
-    $scope.message = "-> loginCtl";
-});
+ function($routeProvider) {
+    $routeProvider.
+       when('/login', {
+          templateUrl: 'login.htm',
+          controller: 'loginCtl'
+       }).
+       when('/dashboard', {
+          templateUrl: 'dashboard.htm',
+          controller: 'dashboardCtl'
+       }).
+       otherwise({
+          redirectTo: '/login'
+       });
+ }]);
 
-app.controller('dashboardCtl', function($scope) {
-    $scope.message = "-> dashboardCtl";
-});
+ app.controller('loginCtl', function($scope) {
+    $scope.message = "LOGIN";
+ });
+
+ app.controller('dashboardCtl', function($scope) {
+    $scope.message = "DAHBOARD";
+ });
