ee6f0e5d2fc3ad9ecdd615956b0a954d48aec0e5
Koding <moritz.goeckel@gmail.com>
2015-08-05 14:31:26 +0000

    Now working

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("app", ['ngRoute']);
+var app = angular.module("app", ['ngRoute', 'ngCookies']);
 
 app.config(['$routeProvider',
  function($routeProvider) {
@@ -16,23 +16,43 @@ app.config(['$routeProvider',
        });
  }]);
 
- app.controller('loginCtl', function($scope, $http, $rootScope) {
+function doLogin($rootScope, $cookies, $location){
+    if($cookies['auth'] == null){
+        $location.path( "/login" );
+        return;
+    }
+    
+    $rootScope.auth = JSON.parse($cookies['auth']);
+    $rootScope.user = JSON.parse($cookies['user']);
+}
+
+ app.controller('loginCtl', function($scope, $http, $rootScope, $location, $cookies) {
     $scope.message = "LOGIN";
-    $scope.submit = function() {
-        $http.post('api.php/login/'+ $scope.name +'/'+ $scope.pw).                  
-          then(function(response) {
-            $rootScope.user = response.data[0];
-            $rootScope.auth = {uid: $rootScope.user.id, secret: $rootScope.user.secret};
-          }, function(response) {console.log("Error: " + response);});
+    if($cookies['auth'] != null){
+        $rootScope.auth = JSON.parse($cookies['auth']);
+        $location.path( "/projects" );
+    }
+    else{
+        $scope.submit = function() {
+            $http.post('api.php/login/'+ $scope.name +'/'+ $scope.pw).                  
+              then(function(response) {
+                    $rootScope.user = response.data[0];
+                    $rootScope.auth = {uid: $rootScope.user.id, secret: $rootScope.user.secret};
+                    $cookies['auth'] = JSON.stringify($rootScope.auth);
+                    $cookies['user'] = JSON.stringify($rootScope.user);
+                    $location.path( "/projects" );
+              }, function(response) {console.log("Error: " + response);});
       };
+    }
  });
 
- app.controller('projectsCtl', function($scope, $http, $rootScope) {
-     console.log($rootScope.auth);
-    $http.post('api.php/user/' + $rootScope.user.id + '/projects/', $rootScope.auth).                  
-          then(function(response) {
-            console.log('api.php/user/' + $rootScope.user.id + '/projects/');
-            console.log(response.data);
-            $rootScope.projects = response.data;
-          }, function(response) {console.log("Error: " + response);});
- });
+ app.controller('projectsCtl', function($scope, $http, $rootScope, $location, $cookies) {
+        doLogin($rootScope, $cookies, $location)
+        
+        $http.post('api.php/user/' + $rootScope.user.id + '/projects/', $rootScope.auth).                  
+            then(function(response) 
+            {
+                $rootScope.projects = response.data;
+            }, 
+            function(response) {console.log("Error: " + response);});
+    });
